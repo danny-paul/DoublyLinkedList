416caf125bdfdfe8eb70749483b90c2bae77f143
danny <70734435+danny-paul@users.noreply.github.com>
2022-01-27 17:25:00 -0500

    Mostly complete; allows duplicates

--- DoublyLinkedList.cpp
+++ DoublyLinkedList.cpp
@@ -3,15 +3,16 @@
 
 
 MyList::MyList() {
-	head = 0;
-	tail = 0;
-	currentPtr = 0;
+	head = NULL;
+	tail = NULL;
+	currentPtr = NULL;
 }
 
 
 MyList::~MyList() {
 	currentPtr = head;
 
+	//use currentPtr to traverse the linked list
 	while (currentPtr != NULL) {
 		head = currentPtr->next; //reassign to new head node
 		free(currentPtr);
@@ -26,27 +27,111 @@ void MyList::insert(int value) {
 }
 
 void MyList::append(int value) {
+	ListNode* nodePtr = new ListNode();
+	nodePtr->value = value;
+	nodePtr->next = NULL;
+	nodePtr->prev = NULL;
 
+	//is list empty?
+	if (head != NULL) {
+		//atleast one node, append new node to end and link appropriately
+		nodePtr->prev = tail;  
+		tail->next = nodePtr;  
+		tail = nodePtr;
+
+	}
+	else {
+		//empty linked list
+		head = nodePtr;
+		tail = nodePtr;
+		currentPtr = nodePtr;
+	}
+
+
+
+	//Yes-->new node linked to head and tail
+	//No-->link to tail node
 }
 
+//adds node to head of linked list
+//O(1)
 void MyList::prepend(int value) {
+	ListNode* nodePtr = new ListNode();
+	nodePtr->value = value;
+	nodePtr->next = NULL;
+	nodePtr->prev = NULL;
 
+	currentPtr = head;
+
+	if (currentPtr != NULL) {
+		//new node becomes front, reassign links of the old front
+		currentPtr->prev = nodePtr;
+		nodePtr->next = currentPtr;
+		head = nodePtr;
+		currentPtr = head;
+	}
+	else {
+		//empty list, assign all to this node
+		head = nodePtr;
+		tail = nodePtr;
+		currentPtr = nodePtr;
+	}
+
+	//empty list
+	
+	//list not empty
+	
 }
 
 void MyList::deleteNode() {
 
 }
 
+//displays the node value that currentPtr is pointing to
 void MyList::displayNode() {
-
+	if (currentPtr != NULL) {
+		//currentPtr-> null
+		std::cout << currentPtr->value << std::endl;
+	}
+	else {
+		//currentPtr-> not null
+		std::cout << "Null" << std::endl;
+	}
 }
 
+//displays nodes in linked list
 void MyList::displayList() {
+	currentPtr = head;
+	int count = 1;
 
+	while (currentPtr != NULL) {
+		std::cout << "#" << count << ": " << currentPtr->value << std::endl;
+		currentPtr = currentPtr->next;
+		count++;
+	}
+
+	currentPtr = head;  //moves back to head
 }
 
+//returns bool if found. If found moves currentPtr to that node. If not found, moved currentPtr to head
+//O(n)
 bool MyList::search(int value) {
-	return true;
+	currentPtr = head;
+
+	if (currentPtr != NULL) {
+		//iterate through nodes (O(n))
+		while (currentPtr != NULL) {
+			if (currentPtr->value == value) {
+				//end iterating and stop currentPtr on the correct node
+				return true;
+			}
+			currentPtr = currentPtr->next;
+		}
+		return false; //nothing found, return false
+	}
+	else {
+		return false;
+	}
 }
 
 
--- main.cpp
+++ main.cpp
@@ -4,7 +4,23 @@
 int main() {
 	MyList list;
 
-	std::cout << "test" << std::endl;
+	list.append(2);
+	list.append(4);
+	list.append(5);
+	list.append(-1);
+	list.append(101);
+	list.append(-1987);
+	list.prepend(999);
+	list.append(989);
+	list.prepend(888);
+	list.displayList();
+
+	std::cout << "list.search(101)" << list.search(101) << std::endl;
+	std::cout << "list.displayNode():";
+	list.displayNode();
+	std::cout << "list.search(9)" << list.search(9) << std::endl;
+	std::cout << "list.displayNode():";
+	list.displayNode();
 
 	return 0;
 }
